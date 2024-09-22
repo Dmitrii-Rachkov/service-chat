87991d54c53e6dd454df95df6278f94f0cc309d5
Dmitriy Rachkov <dsrachkov@avito.ru>
2024-09-22 19:29:12 +0300

    unit-tests - Part 12

--- internal/handler/router_test.go
+++ internal/handler/router_test.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"io"
+	"log/slog"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -77,4 +79,66 @@ func TestNewHandler(t *testing.T) {
 			assert.Equalf(t, handler.services, services, "Expected services: %v\n, Actual: %v", handler.services, services)
 		})
 	}
+}
+
+func TestHandler_NewRouter(t *testing.T) {
+	// Определяем функцию для создания мока сервиса
+	type mockBehaviour func(ctrl *gomock.Controller) *service.Service
+
+	// Тестовая таблица с данными
+	testTable := []struct {
+		name     string
+		mockServ mockBehaviour
+		log      *slog.Logger
+		patterns []string
+	}{
+		{
+			name: "Simple logger",
+			mockServ: func(ctrl *gomock.Controller) *service.Service {
+				return &service.Service{
+					Authorization: mockService.NewMockAuthorization(ctrl),
+					Chat:          mockService.NewMockChat(ctrl),
+					Message:       mockService.NewMockMessage(ctrl),
+				}
+			},
+			log:      slog.New(slog.NewJSONHandler(io.Discard, nil)),
+			patterns: []string{"/auth/*", "/chats/*", "/messages/*", "/swagger/*"},
+		},
+	}
+
+	// Итерируемся по нашей тестовой таблице
+	for _, tt := range testTable {
+		t.Run(tt.name, func(t *testing.T) {
+			//Инициализируем контролер для мока сервиса
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			// Создаём моки сервиса
+			services := tt.mockServ(ctrl)
+
+			// Создаём экземпляр обработчика
+			h := &Handler{
+				services: services,
+			}
+
+			// Смотрим главные маршрутизаторы
+			routes := h.NewRouter(tt.log).Routes()
+
+			// Проверяем, что роутер содержит главные паттерны endPoints
+			for _, route := range routes {
+				assert.True(t, checkPattern(route.Pattern, tt.patterns))
+			}
+		})
+	}
+}
+
+func checkPattern(routePattern string, patterns []string) bool {
+	result := false
+	for i := 0; i < len(patterns); i++ {
+		if routePattern == patterns[i] {
+			result = true
+			break
+		}
+	}
+	return result
 }
